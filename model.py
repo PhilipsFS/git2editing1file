3f972a35f209d9ae31a6cb26389131fb851aede9
philips <philips@fullsail.edu>
2024-05-21 08:34:50 -0400

    Commit

---
+++ model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+
+def generator:
+	#some generator
+	input
+	conv
+	conv	
+	fc
+	fc
+	return gen_model
+
+def discrimiator:
+	#some discriminator
+	input
+	compare
+	create loss
+	return dis_model
